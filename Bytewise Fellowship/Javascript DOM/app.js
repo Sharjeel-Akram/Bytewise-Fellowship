5f95d16e0a634f10916179012ac73330f94a98e0
Sharjeel Akram <sharjeel.akram0200@gmail.com>
2023-04-10 01:04:55 +0500

    Started DOM

---
+++ Bytewise Fellowship/Javascript DOM/app.js
@@ -0,0 +1,11 @@
+// GetElimentById()
+const title = document.getElementById('main-heading');
+console.log(title);
+
+// GetelemneByClassName()
+// const listItem = document.getElementsByClassName('list-items');
+// console.log(listItem);
+
+// GetElementByTagName()
+const listItem = document.getElementsByTagName('list-items');
+console.log(listItem);
