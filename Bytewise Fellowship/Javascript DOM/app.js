9c6b5fc22f25e6a06d934e36fae3aac79ecfbff5
Sharjeel Akram <sharjeel.akram0200@gmail.com>
2023-04-10 12:59:55 +0500

    update

--- Bytewise Fellowship/Javascript DOM/app.js
+++ Bytewise Fellowship/Javascript DOM/app.js
@@ -1,11 +1,20 @@
 // GetElimentById()
-const title = document.getElementById('main-heading');
-console.log(title);
+// const title = document.getElementById('main-heading');
+// console.log(title);
 
 // GetelemneByClassName()
 // const listItem = document.getElementsByClassName('list-items');
 // console.log(listItem);
 
 // GetElementByTagName()
-const listItems = document.getElementsByTagName('li');
-console.log(listItems);
+// const listItems = document.getElementsByTagName('li');
+// console.log(listItems);
+
+// QUery Selector
+// const container = document.querySelector('dic');
+// console.log(container);
+
+//Query SelectotAll()
+const container = document.querySelectorAll('dic');
+console.log(container);
+
